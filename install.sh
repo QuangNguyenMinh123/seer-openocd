92580044b4c05dcc1041277697c4929d231324fa
QuangNguyenMinh123 <100016403+QuangNguyenMinh123@users.noreply.github.com>
2025-08-02 14:16:36 +0700

    update build script

--- install.sh
+++ install.sh
@@ -1,18 +1,23 @@
 set -x
+#########################################################
+SEER_VER=2.5
+TOP_DIR=${PWD}
+#########################################################
 sudo rm -rf output/*
 sudo rm -f /usr/bin/seergdb
 git clone https://github.com/epasveer/seer.git
-cd seer
-git checkout tags/v2.4
-cd src
+sudo apt install -y debhelper dh-exec qt6-base-dev libqt6charts*-dev libqt6svg*-dev
+cd ${TOP_DIR}/seer
+git checkout tags/v${SEER_VER}
+cd ${TOP_DIR}/seer/src
 mkdir build
-cd build
+cd ${TOP_DIR}/seer/src/build
 cmake ..
-make seergdb -j4
-cd ../..
-sudo dpkg-buildpackage -j4
-cd ..
+make seergdb -j8
+cd ${TOP_DIR}/seer
+sudo dpkg-buildpackage -j8
+cd ${TOP_DIR}
 sudo cp seergdb.desktop /usr/share/applications/
-sudo dpkg -i seergdb_2.4_amd64.deb
-cp -rf seergdb* output
-cp -rf seergdb_2.4_amd64.deb output
+sudo dpkg -i seergdb_${SEER_VER}_amd64.deb
+cd ${TOP_DIR}/seer/src/build
+sudo make install
